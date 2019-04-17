44896cfcafce31465a4f9c531c6c373689321d33
Hugo <hugo_val28@hotmail.com>
2019-04-16 23:15:05 -0600

    fix logs

--- src/api/log/controller.js
+++ src/api/log/controller.js
@@ -3,20 +3,13 @@ import { Log } from '.'
 
 export const create = log => Log.create(log).then(log => log.view(true))
 
-export const index = ({ querymen: { query, select, cursor } }, res, next) => {
-  console.log('TCL: index -> query', query)
-  console.log('TCL: index -> select', select)
-  console.log('TCL: index -> cursor', cursor)
-
-  return Log.count(query)
+export const index = ({ querymen: { query, select, cursor } }, res, next) =>
+  Log.count(query)
     .then(count =>
-      Log.find(query, select, cursor)
-        .populate('product')
-        .then(logs => ({
-          count,
-          rows: logs.map(log => log.view())
-        }))
+      Log.find(query, select, { ...cursor, limit: 0 }).then(logs => ({
+        count,
+        rows: logs.map(log => log.view())
+      }))
     )
     .then(success(res))
     .catch(next)
-}
