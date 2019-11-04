305df07d1f74f444ada7399e6c0ca36a95d368a4
Hugo Licon <hugo_val28@hotmail.com>
2019-11-03 21:04:41 -0700

    set user agent

--- src/services/scraper/index.js
+++ src/services/scraper/index.js
@@ -2,7 +2,13 @@ import axios from 'axios'
 import cheerio from 'cheerio'
 
 export async function getHTML (url) {
-  const response = await axios.get(url, {timeout: 5000}).catch(e => {
+  const response = await axios.get(url, {
+    timeout: 5000,
+    headers: {
+      'User-Agent':
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'
+    }
+  }).catch(e => {
     console.log('Error getting html', e)
     return Promise.reject(e)
   })
