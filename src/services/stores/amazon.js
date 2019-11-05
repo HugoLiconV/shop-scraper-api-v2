db1d604e5a070ffa4525f8759dd95d88a5fb432a
Hugo Licon <hugo_val28@hotmail.com>
2019-11-04 23:48:01 -0700

    fix amazon scraper

--- src/services/stores/amazon.js
+++ src/services/stores/amazon.js
@@ -7,9 +7,20 @@ const imageAttribute = 'data-old-hires'
 
 export default function scrapAmazonProduct (html) {
   const image = scraper.scrapImage(html, imageSelector, imageAttribute)
+  const title = scraper.scrapText(html, productNameSelector)
   const price = scraper.currencyStringToNumber(
     scraper.scrapText(html, priceSelector)
   )
-  const title = scraper.scrapText(html, productNameSelector)
+  if (!price || price === 0) {
+    // sometimes the selector change if there is a discount in the product
+    const newPrice = scraper.currencyStringToNumber(
+      scraper.scrapText(html, '#priceblock_dealprice')
+    )
+    return {
+      image,
+      title,
+      price: newPrice
+    }
+  }
   return { image, title, price }
 }
